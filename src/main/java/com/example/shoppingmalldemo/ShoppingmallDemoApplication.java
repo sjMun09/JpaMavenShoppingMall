7848af93107c9c1eb0d432e95ca5223db56536f3
sjMun09 <ohoh7391@naver.com>
2024-03-06 22:08:43 +0900

    bug: 1_jpaQuery 확인x, 2_ItemRepository 실행시 빈 찾을 수 없는 에러 발생

--- src/main/java/com/example/shoppingmalldemo/ShoppingmallDemoApplication.java
+++ src/main/java/com/example/shoppingmalldemo/ShoppingmallDemoApplication.java
@@ -2,12 +2,13 @@ package com.example.shoppingmalldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.example.controller"}) // 컨트롤러를 인식 못해서 어노테이션 추가
 public class ShoppingmallDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingmallDemoApplication.class, args);
 	}
-
 }
