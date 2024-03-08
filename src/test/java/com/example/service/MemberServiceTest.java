40aaa825cf2649f9ffec6f09e4f86e307ae3cb40
sjMun09 <ohoh7391@naver.com>
2024-03-09 08:13:18 +0900

    feat: 회원가입 form 작성

--- src/test/java/com/example/service/MemberServiceTest.java
+++ src/test/java/com/example/service/MemberServiceTest.java
@@ -34,6 +34,9 @@ class MemberServiceTest {
         return Member.createMember(memberFormDto, passwordEncoder);
     }
 
+    /**
+     * JUnit 테스트 코드
+     */
     @Test
     @DisplayName("회원가입 테스트")
     public void saveMemberTest() {
@@ -45,5 +48,35 @@ class MemberServiceTest {
         assertEquals(member.getPassword(), savedMember.getPassword());
         assertEquals(member.getAddress(), savedMember.getAddress());
         assertEquals(member.getRole(), savedMember.getRole());
+    }
+    /**
+     * AssertJ 테스트 코드 -> 사용하려면 의존성 주입 필요함.
+     */
+    /*@Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest() {
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        assertThat(savedMember.getName()).isEqualTo(member.getName());
+        assertThat(savedMember.getEmail()).isEqualTo(member.getEmail());
+        assertThat(savedMember.getPassword()).isEqualTo(member.getPassword());
+        assertThat(savedMember.getAddress()).isEqualTo(member.getAddress());
+        assertThat(savedMember.getRole()).isEqualTo(member.getRole());
+    }*/
+
+    /**
+     * 중복 회원가입 검증
+     */
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicateMemberTest() {
+        Member member1 = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);}
+        );
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
     }
 }
